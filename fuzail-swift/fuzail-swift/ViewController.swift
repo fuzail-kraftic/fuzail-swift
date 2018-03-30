9fcb4df2fcd0841833252387976eefa13069b577
Luffy <luffy@kaizoku.com>
2018-03-30 23:13:22 +0530

    second commit

---
+++ fuzail-swift/fuzail-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fuzail-swift
+//
+//  Created by Fuzail Ahmed on 30/03/18.
+//  Copyright © 2018 Kraftic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
